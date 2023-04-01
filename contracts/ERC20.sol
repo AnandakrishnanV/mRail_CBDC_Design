64226342a65e1ce9782e860d058bef689c8e97d8
Pitchappan P RM <46064869+Pitch2342@users.noreply.github.com>
2023-04-01 14:45:23 +0800

    To Implement

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -16,3 +16,34 @@ contract ERC20 is Ownable, IERC20 {
 
 }
 
+Contract CentralBank
+//     extension of ERC20 or self coded
+//     Attributes
+//         Name
+//         OwnerWallet
+//         CBDC_Balance_Array
+//         CBDCMapping
+//         Primary_Retail_Users_Array
+//         Secondary_Retail_Users_Array
+        
+//         ERC20 vals
+
+//     Functions
+//         ERC20 Functions
+
+//         CreateUserObj
+//         TransferUserObj
+//         AllowSecondaryAccess
+//         Blacklist
+//         Whitelist
+
+// Contract Retail User
+//     Attributes
+//         Owner
+//         PrimaryID
+//         SecondaryIDs
+//         CBDCMapping
+//     Functions
+//         PrintBalances
+//         TranferCurrency
+//         TranferObject
