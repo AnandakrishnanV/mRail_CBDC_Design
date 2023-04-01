a22172675617fa36b4fd475c403bb840a6d199f9
Pitchappan P RM <46064869+Pitch2342@users.noreply.github.com>
2023-04-01 16:53:05 +0800

    try Apr1 16:52

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,24 +2,62 @@
 
 pragma solidity ^0.8.0;
 
-import './interfaces/IERC20.sol';
-import "./ownership/Ownable.sol";
+interface IERC20 {
+    function balanceOf(address who) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    // function allowance(address owner, address spender) external view returns (uint256);
+    // function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
 
-contract ERC20 is Ownable, IERC20 {
+contract Ownable {
+  address public owner;
+
+    event TransferOwnership(address formerOwner, address newOwner);
+
+  constructor(){
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  function transferOwnership(address _newOwner) public virtual onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+ 
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0), 'Ownable: address is not valid');
+    owner = _newOwner;
+    emit TransferOwnership(msg.sender, _newOwner);
+  }
+}
+
+contract CBDC is Ownable, IERC20 {
 
     string public name;
-    string public symbol;
+    string public currency;
     uint8 public decimals;
     uint256 public totalSupply;
 
     mapping (address => uint256) internal _balances;
     mapping (address => mapping (address => uint256)) internal _allowed;
 
-    constructor (string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
-        symbol = _symbol;
+    mapping (string => address) internal primaryRetailUsers;
+
+    mapping (uint256 => string) internal UserMapping;
+    uint256 public numUsers;
+
+    constructor (string memory _name, string memory _currency, uint8 _decimals, uint256 _totalSupply) {
+        currency = _currency;
         name = _name;
         decimals = _decimals;
         _balances[msg.sender] = _totalSupply;
+        totalSupply = _totalSupply;
+        
     }
 
     function transfer(address _to, uint256 _value) external override returns (bool) {
@@ -32,58 +70,76 @@ contract ERC20 is Ownable, IERC20 {
         return true;
     }
 
-    function balanceOf(address _owner) external override view returns (uint256 balance) {
-        return _balances[_owner];
+    function balanceOf(address _addr) external override view returns (uint256 balance) {
+        // only u and owner access balance
+        return _balances[_addr];
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) external override returns (bool) {
-        require(_from != address(0), 'ERC20: from address is not valid');
-        require(_to != address(0), 'ERC20: to address is not valid');
-        require(_value <= _balances[_from], 'ERC20: insufficient balance');
-        require(_value <= _allowed[_from][msg.sender], 'ERC20: transfer from value not allowed');
-
-        _allowed[_from][msg.sender] = _allowed[_from][msg.sender] - _value;
-        _balances[_from] = _balances[_from] - _value;
-        _balances[_to] = _balances[_to] + _value;
-                
-        return true;
-   }
-
-   function allowance(address _owner, address _spender) external override view returns (uint256) {
-        return _allowed[_owner][_spender];
-    }
-
-    function mintTo(address _to,uint256 _amount) external onlyOwner returns (bool) {
-        require(_to != address(0), 'ERC20: to address is not valid');
-
-        _balances[_to] = _balances[_to] + _amount;
+    function mint(uint256 _amount) external onlyOwner returns (bool) {
+        
+        _balances[owner] = _balances[owner] + _amount;
         totalSupply = totalSupply + _amount;
 
         return true;
     }
 
-    function burn(uint256 _amount) external returns (bool) {
-        require(_balances[msg.sender] >= _amount, 'ERC20: insufficient balance');
-
-        _balances[msg.sender] = _balances[msg.sender] - _amount;
-        totalSupply = totalSupply - _amount;
-
-        return true;
-    }
-
-    function burnFrom(address _from,uint256 _amount) external returns (bool) {
-        require(_from != address(0), 'ERC20: from address is not valid');
-        require(_balances[_from] >= _amount, 'ERC20: insufficient balance');
-        require(_amount <= _allowed[_from][msg.sender], 'ERC20: burn from value not allowed');
-        
-        _allowed[_from][msg.sender] = _allowed[_from][msg.sender] - _amount;
-        _balances[_from] = _balances[_from] - _amount;
-        totalSupply = totalSupply - _amount;
+    function createRetailUser(string memory _name, address _newOwner) external onlyOwner  returns(bool){
+        RetailUser retailUser = new RetailUser(
+                                        _name,
+                                        name,
+                                        msg.sender,
+                                        currency
+                                    );
+        primaryRetailUsers[_name] = address(retailUser);
+        retailUser.transferOwnership(_newOwner);
 
         return true;
     }
 
+    function accessUserAddress(string memory _name) external view onlyOwner returns (address) {
+        address temp = primaryRetailUsers[_name];
+        return temp;
+    }
 
+
+
+}
+
+contract RetailUser is Ownable {
+    string public name;
+    string public CreatorName;
+    address public CreatorAddress;
+    string public PrimaryCurrency;
+    mapping (string => address) public CBDCAddresses;
+    // hold all currencies string[] currencies;
+    // mapping (address => string) public Certificates;
+
+    constructor (
+        string memory _name, 
+        string memory _creatorName,
+        address _creatorAddr,
+        string memory _primaryCurrency
+        ) {
+        name = _name;
+        CreatorName = _creatorName;
+        CreatorAddress = _creatorAddr;
+        PrimaryCurrency = _primaryCurrency;
+        CBDCAddresses[_primaryCurrency] = _creatorAddr;
+        //hold currencies
+        //hold certificates
+    }
+
+    function transferCurrency(string memory _currency, address _to, uint256 _value) public {
+        address currency_addr = CBDCAddresses[_currency];
+        CBDC tempAcces = CBDC(currency_addr);
+        tempAcces.transfer(_to, _value);
+    }
+
+    function printBalance(string memory _currency) public view returns (uint256 balance){
+        address currency_addr = CBDCAddresses[_currency];
+        CBDC tempAcces = CBDC(currency_addr);
+        return tempAcces.balanceOf(address(this));
+    }
 }
 
 // Contract CentralBank
